670a076a63ed1139a70d7588e7aba524cd376c3f
JB-hb <jbhbprograming@gmail.com>
2025-04-22 18:33:51 -0400

    now init

--- main/ci_comunication.c
+++ main/ci_comunication.c
@@ -6,12 +6,16 @@
 #include "esp_netif.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
+#include "esp_log.h"
 
 esp_err_t init_now();
 esp_err_r init_wifi();
 
 void app_main(void)
 {
+
+	ESP_ERROR_CHECK(init_wifi());
+	ESP_ERROR_CHECK(init_now());
 
 }
 
@@ -28,5 +32,17 @@ esp_err_t init_wifi(){
 	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_FLASH));
 	ESP_ERROR_CHECK(esp_wifi_start());
 
+	ESP_LOGI("wifi", "wifi initialized");
+
 	return ESP_OK;
+}
+
+esp_err_t init_now(){
+
+	esp_now_init();
+	//Todo: insert recv callback function
+	//Todo: insert send callback function
+
+	return ESP_OK
+
 }
